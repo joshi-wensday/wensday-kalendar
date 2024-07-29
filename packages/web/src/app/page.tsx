564a1ee1cc9f1c766a96f81a464ee377b3741cbd
joshi-wensday <joshi@wensday.org>
2024-07-29 20:12:25 +0100

    minor changes, decided to go back to shadcnv1-broken branch

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Welcome to Wensday Kalendar</h1>;
+  return (
+    <div className="bg-background text-foreground p-4">
+      <h1 className="text-2xl font-bold">Welcome to Wensday Kalendar</h1>
+    </div>
+  );
 }
